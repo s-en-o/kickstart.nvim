ff3af015b38fd2ce85d11d0312a30272a3bbab88
Seno <seno.is.dev@gmail.com>
2024-10-10 18:23:25 +1300

    add lualine, wrapping

--- init.lua
+++ init.lua
@@ -316,7 +316,8 @@ require('lazy').setup({
     require 'kickstart.plugins.nvim-tree',
     require 'kickstart.plugins.theme',
     require 'kickstart.plugins.autoformat',
-
+    require 'kickstart.plugins.wrapping',
+    require 'kickstart.plugins.lualine',
     -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
     --    This is the easiest way to modularize your config.
     --
@@ -326,6 +327,15 @@ require('lazy').setup({
 
     -- Useful cheatsheet
     -- find and replacing text :%s@search@replace@gc
+    --
+    -- Comment multiple line
+    --1. select the first caracter of your block
+    --2. press Ctrl+V ( this is rectangular visual selection mode)
+    --3. type j for each line more you want to be commented
+    --4. type Shift-i (like I for "insert at start")
+    --5. type // (or # or " or ...)
+    --6. you will see the modification appearing only on the first line
+    --7. IMPORTANT LAST STEP: type Esc key, and there you see the added character appear on all lines
 }, {
     ui = {
         -- If you are using a Nerd Font: set icons to an empty table which will use the
---
+++ lua/kickstart/plugins/wrapping.lua
@@ -0,0 +1,8 @@
+return {
+    'andrewferrier/wrapping.nvim',
+    config = function()
+        require('wrapping').setup()
+
+        require('wrapping').soft_wrap_mode()
+    end,
+}
